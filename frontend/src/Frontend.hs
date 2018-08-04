abb186ffa0cb4bd9261e1febe2f5d5eb47e6b25d
blargg <tomjankauski@gmail.com>
2018-08-04 22:48:00 +0000

    css styling

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -6,20 +6,22 @@ module Frontend where
 import qualified Data.Text as T
 import Reflex.Dom.Core
 import Reflex.Dom
-import Control.Lens
 import Data.Map (Map)
 import qualified Data.Map as M
 import Text.Read (readMaybe)
 
 import Common.Api
 import Data.CharacterSheet
+import Frontend.Input
 import Static
 
 frontend :: (StaticWidget x (), Widget x ())
 frontend = (header, body)
 
 header :: StaticWidget x ()
-header = el "title" $ text "Character Sheet"
+header = do
+    el "title" $ text "Character Sheet"
+    elAttr "link" (M.fromList [("rel", "stylesheet"), ("href", static @"css/Style.css")]) $ return ()
 
 body :: Widget x ()
 body = do
@@ -27,17 +29,10 @@ body = do
   el "p" $ text $ T.pack commonStuff
   val <- fmap (maybe 0 id) <$> numberInput
   text <- textInput def
-  let character = invert $ CharacterSheet val (value text)
+  let character = invertId $ CharacterSheet val (value text)
   display character
   return ()
 
-
--- TODO set a class depending on valid or invalid
-numberInput :: MonadWidget t m => m (Dynamic t (Maybe Int))
-numberInput = do
-    text <- textInput numberConfig
-    return $ fmap parse $ value text
-    where
-        numberConfig = def & textInputConfig_inputType .~ "number"
-        parse :: T.Text -> Maybe Int
-        parse = readMaybe . T.unpack
+abilityDisplay :: (MonadWidget t m) => T.Text -> m (Dynamic t Int)
+abilityDisplay name = undefined
+    --abilityScore <- numberInput
