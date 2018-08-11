ed922a76a6ec44433f3fea7bb6c69aac525fc504
blargg <tomjankauski@gmail.com>
2018-08-11 19:24:01 +0000

    Add applicative compose operator

--- common/src/Common/Compose.hs
+++ common/src/Common/Compose.hs
@@ -1,6 +1,11 @@
 module Common.Compose where
 
+import Data.Functor.Compose
+
 infixl 4 <$$>
 
 (<$$>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
 fn <$$> x = fmap (fmap fn) x
+
+(<**>) :: (Applicative f, Applicative g) => f (g (a -> b)) -> f (g a) -> f (g b)
+fn <**> x = getCompose $ Compose fn <*> Compose x
