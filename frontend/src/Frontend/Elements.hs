916ba1546e066483fa46ad1a57d62f6157ede2c4
blargg <tomjankauski@gmail.com>
2019-05-31 21:38:38 -0500

    Fix build issues from modules

--- frontend/src/Frontend/Elements.hs
+++ frontend/src/Frontend/Elements.hs
@@ -2,28 +2,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Frontend.Elements
-    (urlLink
-    ,div
-    ,divC
-    ,span
+    ( urlLink
+    , div
+    , divC
+    , span
     )
         where
+
+import Prelude hiding (div, span)
+import Reflex.Dom
+import Data.Text (Text)
 
 -- | This module is meant to be imported qualified, many of the names are short,
 -- and conflict with common operations.
 --
--- For example:
--- ```
--- import qualified Frontend.Elements as E
--- E.div $ text "hello div"
--- ```
---
 -- el "tag" -> tag
 -- elAttr "tag" -> tagA
 -- elClass "tag" -> tagC
-import Prelude hiding (div, span)
-import Reflex.Dom
-import Data.Text (Text)
 
 urlLink :: DomBuilder t m => Text -> Text -> m ()
 urlLink url displayText = elAttr "a" ("href" =: url) (text displayText)
